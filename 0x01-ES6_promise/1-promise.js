efa7009082cc78446e0f7937138a7891d78d50be
Frankenstein9585 <donny9585@gmail.com>
2023-07-26 14:38:25 +0100

    Done task 1

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,17 @@
+export default function getResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    const data = {
+      status: 200,
+      body: 'Success',
+    };
+    const error = {
+      Error: 'The fake API is not working currently',
+    };
+
+    if (!success) {
+      reject(error);
+    } else {
+      resolve(data);
+    }
+  });
+}
