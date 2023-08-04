e79f170b795eb3bf0c79e50f07a4079e7d8f161c
Frankenstein9585 <donny9585@gmail.com>
2023-08-04 23:20:38 +0100

    Modified task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,12 @@
 export default function cleanSet(set, startString) {
-  let string = '';
-  set.forEach((element) => {
-    if (element.startsWith(startString) && startString !== '') {
-      string += element.split(startString).join('-');
+  const newList = [];
+  if (startString === '' || typeof startString !== 'string') {
+    return '';
+  }
+  for (const prop of set) {
+    if ((typeof prop === 'string') && (prop.startsWith(startString))) {
+      newList.push(prop.slice(startString.length));
     }
-  });
-
-  return string.substring(1, string.length);
+  }
+  return newList.join('-');
 }
