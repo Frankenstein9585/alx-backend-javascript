a70ec76f966fcdbd8ae68169c25be7997ee5947e
Frankenstein9585 <donny9585@gmail.com>
2023-09-18 21:47:04 +0100

    Changed to synchronous

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,41 +1,69 @@
-const readline = require('readline');
+// const readline = require('readline');
 const fs = require('fs');
 
 function countStudents(path) {
-  fs.access(path, fs.constants.F_OK, (err) => {
-    if (err) {
-      throw new Error('Cannot load the database');
-    }
-  });
-  const readInterface = readline.createInterface({
-    input: fs.createReadStream(path),
-    output: process.stdout,
-    terminal: false,
-  });
+  // fs.access(path, fs.constants.F_OK, (err) => {
+  //   if (err) {
+  //     throw new Error('Cannot load the database');
+  //   }
+  // });
+  // const readInterface = readline.createInterface({
+  //   input: fs.createReadStream(path),
+  //   output: process.stdout,
+  //   terminal: false,
+  // });
+  //
+  // let studentsCount = 0;
+  // let CSCount = 0;
+  // let SWECount = 0;
+  // const CSList = [];
+  // const SWEList = [];
+  //
+  // readInterface.on('line', (line) => {
+  //   const firstName = line.split(',')[0];
+  //   studentsCount += 1;
+  //   if (line.includes('CS')) {
+  //     CSCount += 1;
+  //     CSList.push(firstName);
+  //   } else if (line.includes('SWE')) {
+  //     SWECount += 1;
+  //     SWEList.push(firstName);
+  //   }
+  // });
+  //
+  // readInterface.on('close', () => {
+  //   console.log(`Number of students: ${studentsCount - 1}`);
+  //   console.log(`Number of students in CS: ${CSCount}. List: ${CSList.join(', ')}`);
+  //   console.log(`Number of students in SWE: ${SWECount}. List: ${SWEList.join(', ')}`);
+  // });
+  try {
+    let studentsCount = 0;
+    let CSCount = 0;
+    let SWECount = 0;
+    const CSList = [];
+    const SWEList = [];
+    const data = fs.readFileSync(path, 'utf-8').split('\n');
 
-  let studentsCount = 0;
-  let CSCount = 0;
-  let SWECount = 0;
-  const CSList = [];
-  const SWEList = [];
+    data.shift();
 
-  readInterface.on('line', (line) => {
-    const firstName = line.split(',')[0];
-    studentsCount += 1;
-    if (line.includes('CS')) {
-      CSCount += 1;
-      CSList.push(firstName);
-    } else if (line.includes('SWE')) {
-      SWECount += 1;
-      SWEList.push(firstName);
-    }
-  });
+    data.forEach((line) => {
+      const firstName = line.split(',')[0];
+      studentsCount += 1;
+      if (line.includes('CS')) {
+        CSCount += 1;
+        CSList.push(firstName);
+      } else if (line.includes('SWE')) {
+        SWECount += 1;
+        SWEList.push(firstName);
+      }
+    });
 
-  readInterface.on('close', () => {
-    console.log(`Number of students: ${studentsCount - 1}`);
+    console.log(`Number of students: ${studentsCount}`);
     console.log(`Number of students in CS: ${CSCount}. List: ${CSList.join(', ')}`);
     console.log(`Number of students in SWE: ${SWECount}. List: ${SWEList.join(', ')}`);
-  });
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;
