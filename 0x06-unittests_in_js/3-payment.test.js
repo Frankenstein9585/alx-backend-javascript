99bc9dc5102d8c3495bc4a21256fea40443b2d8f
Donald Peters <donny9585@gmail.com>
2023-09-20 21:42:14 +0100

    Restored spies after each check

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -3,14 +3,17 @@ const sinon = require('sinon');
 const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
-    let spy = sinon.spy(Utils, "calculateNumber");
     it('checks that calculateNumber was called',  () => {
+        let spy = sinon.spy(Utils, "calculateNumber");
         sendPaymentRequestToApi(100, 20);
         sinon.assert.calledOnce(spy);
+        spy.restore();
     });
 
     it('checks that calculateNumber was called with correct parameters',  () => {
+        let spy = sinon.spy(Utils, "calculateNumber");
         sendPaymentRequestToApi(100, 20);
         sinon.assert.calledWith(spy, 'SUM', 100, 20);
+        spy.restore();
     });
 });
