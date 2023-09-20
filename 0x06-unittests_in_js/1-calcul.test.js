6336ba1c11ac29c4772dda08eabc8c1b23670da1
Donald Peters <donny9585@gmail.com>
2023-09-20 19:03:37 +0100

    Required correct JS file

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,37 +1,37 @@
 const calculateNumber = require('./1-calcul.js');
 const assert = require('assert');
 
-describe('calculateNumber: ADD', () => {
+describe('calculateNumber: SUM', () => {
     it('Two whole numbers', () => {
-        assert.equal(calculateNumber(1, 4), 5);
+        assert.equal(calculateNumber('SUM',1, 4), 5);
     });
 
     it('One negative one positive', () => {
-        assert.equal(calculateNumber(-4, 2), -2);
+        assert.equal(calculateNumber('SUM',-4, 2), -2);
     });
 
     it('One positive one negative', () => {
-        assert.equal(calculateNumber(4, -2), 2);
+        assert.equal(calculateNumber('SUM',4, -2), 2);
     });
 
     it('Two positive decimals', () => {
-        assert.equal(calculateNumber(1.5, 3.7), 6);
+        assert.equal(calculateNumber('SUM',1.5, 3.7), 6);
     });
 
     it('Two positive decimals(<.5)', () => {
-        assert.equal(calculateNumber(1.4, 3.7), 5);
+        assert.equal(calculateNumber('SUM',1.4, 3.7), 5);
     });
 
     it('One decimal one whole', () => {
-        assert.equal(calculateNumber(3.5, 3), 7);
+        assert.equal(calculateNumber('SUM',3.5, 3), 7);
     });
 
     it('One whole one decimal', () => {
-        assert.equal(calculateNumber(3, 3.5), 7);
+        assert.equal(calculateNumber('SUM',3, 3.5), 7);
     });
 
     it('Two negative decimals', () => {
-        assert.equal(calculateNumber(-1.5, -1.6), -3);
+        assert.equal(calculateNumber('SUM',-1.5, -1.6), -3);
     });
 
 });
