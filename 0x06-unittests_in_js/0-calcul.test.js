6f05ededaa9c407340e9ff4ca44ba33c102cb642
Donald Peters <donny9585@gmail.com>
2023-09-20 18:19:42 +0100

    Added more tests to task 1

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,23 +2,35 @@ const calculateNumber = require('./0-calcul.js');
 const assert = require('assert');
 
 describe('calculateNumber', () => {
-    it('should return 5', () => {
+    it('Two whole numbers', () => {
         assert.equal(calculateNumber(1, 4), 5);
     });
 
-    it('should return -2', () => {
+    it('One negative one positive', () => {
         assert.equal(calculateNumber(-4, 2), -2);
     });
 
-    it('should return 6', () => {
+    it('One positive one negative', () => {
+        assert.equal(calculateNumber(4, -2), 2);
+    });
+
+    it('Two positive decimals', () => {
         assert.equal(calculateNumber(1.5, 3.7), 6);
     });
 
-    it('should return 7', () => {
+    it('Two positive decimals(<.5)', () => {
+        assert.equal(calculateNumber(1.4, 3.7), 5);
+    });
+
+    it('One decimal one whole', () => {
         assert.equal(calculateNumber(3.5, 3), 7);
     });
 
-    it('should return -3', () => {
+    it('One whole one decimal', () => {
+        assert.equal(calculateNumber(3, 3.5), 7);
+    });
+
+    it('Two negative decimals', () => {
         assert.equal(calculateNumber(-1.5, -1.6), -3);
     });
 });
