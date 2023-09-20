942319ed08ea8b8a86fb48707f6dd192612221cb
Donald Peters <donny9585@gmail.com>
2023-09-20 18:14:29 +0100

    Added more tests to task 1

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -7,19 +7,15 @@ describe('calculateNumber', () => {
     });
 
     it('should return -2', () => {
-        assert.equal(calculateNumber(-4, 2), 2);
+        assert.equal(calculateNumber(-4, 2), -2);
     });
 
     it('should return 6', () => {
         assert.equal(calculateNumber(1.5, 3.7), 6);
     });
 
-    it('should return 6', () => {
-        assert.equal(calculateNumber(1.5, 3.7), 6);
-    });
-
-    it('should return -2', () => {
-        assert.equal(calculateNumber(-1.5, -1.5), -2);
+    it('should return 7', () => {
+        assert.equal(calculateNumber(3.5, 3), 7);
     });
 
     it('should return -3', () => {
