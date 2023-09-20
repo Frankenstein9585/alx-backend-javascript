f65649a40765671f9b8a417493be9559ec40e29a
Donald Peters <donny9585@gmail.com>
2023-09-20 19:00:08 +0100

    Required correct JS file

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -3,19 +3,35 @@ const assert = require('assert');
 
 describe('calculateNumber: ADD', () => {
     it('Two whole numbers', () => {
-        assert.equal(calculateNumber('SUM',1, 4), 5);
+        assert.equal(calculateNumber(1, 4), 5);
     });
 
     it('One negative one positive', () => {
-        assert.equal(calculateNumber('SUM',-4, 2), -2);
+        assert.equal(calculateNumber(-4, 2), -2);
     });
 
-    it('Two decimals', () => {
-        assert.equal(calculateNumber('SUM',1.5, 3.7), 6);
+    it('One positive one negative', () => {
+        assert.equal(calculateNumber(4, -2), 2);
+    });
+
+    it('Two positive decimals', () => {
+        assert.equal(calculateNumber(1.5, 3.7), 6);
+    });
+
+    it('Two positive decimals(<.5)', () => {
+        assert.equal(calculateNumber(1.4, 3.7), 5);
     });
 
     it('One decimal one whole', () => {
-        assert.equal(calculateNumber('SUM', 3.5, 3), 7);
+        assert.equal(calculateNumber(3.5, 3), 7);
+    });
+
+    it('One whole one decimal', () => {
+        assert.equal(calculateNumber(3, 3.5), 7);
+    });
+
+    it('Two negative decimals', () => {
+        assert.equal(calculateNumber(-1.5, -1.6), -3);
     });
 
 });
